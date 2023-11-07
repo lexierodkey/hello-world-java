b93b553907cef4563b6bf2985ccfdb62ff468989
Lexie-Anne Rodkey <lexiea.rodkey@gmail.com>
2023-11-07 13:44:31 -0500

    I changed my name to Lexie and then Lexie-Anne

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world!");
-    System.out.println("My name is Lexie"); 
+    System.out.println("My name is Lexie-Anne"); 
   }
 }
